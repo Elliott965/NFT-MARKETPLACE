2e87c6324b6cec153aa487a8d4038f463b513df0
max <95427253+Elliott965@users.noreply.github.com>
2023-12-20 10:51:35 +0530

    Add files via upload

---
+++ src/pinata.js
@@ -0,0 +1,91 @@
+//require('dotenv').config();
+const key = process.env.REACT_APP_PINATA_KEY;
+const secret = process.env.REACT_APP_PINATA_SECRET;
+
+const axios = require('axios');
+const FormData = require('form-data');
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: "f334aa4b4e8b637a574b",
+                pinata_secret_api_key: "7a00aee1d8a194dd477bacf4c827cd233d703a93c54c7873e00b108942cb5dec",
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
+export const uploadFileToIPFS = async(file) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    
+    let data = new FormData();
+    data.append('file', file);
+
+    const metadata = JSON.stringify({
+        name: 'testname',
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+    });
+    data.append('pinataMetadata', metadata);
+
+    //pinataOptions are optional
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+        customPinPolicy: {
+            regions: [
+                {
+                    id: 'FRA1',
+                    desiredReplicationCount: 1
+                },
+                {
+                    id: 'NYC1',
+                    desiredReplicationCount: 2
+                }
+            ]
+        }
+    });
+    data.append('pinataOptions', pinataOptions);
+
+    return axios 
+        .post(url, data, {
+            maxBodyLength: 'Infinity',
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: "f334aa4b4e8b637a574b" ,
+                pinata_secret_api_key: "7a00aee1d8a194dd477bacf4c827cd233d703a93c54c7873e00b108942cb5dec",
+            }
+        })
+        .then(function (response) {
+            console.log("image uploaded", response.data.IpfsHash)
+            return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
